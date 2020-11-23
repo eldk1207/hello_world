6e104147a4856a5eec41db62e10302f890904aea
eldk1 <eldk1@LAPTOP-CP7IR8OF>
2020-11-23 22:36:48 +0900

    세번쨰

---
+++ src/excep_p/ForMain.java
@@ -0,0 +1,22 @@
+package excep_p;
+
+public class ForMain {
+	public static void main(String[] args) {
+		
+		
+		try {
+			
+			
+			for (int i = 0; i < 5; i++) {
+				System.out.println("try > for"+i);
+			}
+		
+		
+		}catch (Exception e) {
+			
+		}
+		System.out.println("try > for Á¾·á-------------");
+		
+		
+	}
+}
